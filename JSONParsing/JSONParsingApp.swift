b9eada21f3b59f6c9305d5f5e09991024994adb1
Atikur Rahman <mratikurrahman@gmail.com>
2023-10-16 23:30:39 +0600

    Project was created

---
+++ JSONParsing/JSONParsingApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSONParsingApp.swift
+//  JSONParsing
+//
+//  Created by Atikur Rahman on 10/16/23.
+//
+
+import SwiftUI
+
+@main
+struct JSONParsingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
